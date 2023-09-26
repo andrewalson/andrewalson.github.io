f8ac4cbea16c0985a6744400b2e02e2831f29b31
andrewalson <83783605+andrewalson@users.noreply.github.com>
2023-09-25 22:08:52 -0400

    Add nav bar and footer scaffoldings

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<nav>
+    <a prefetch href="/">Nav bar</a>
+</nav>
+
+<slot /> 
+
+<footer>Footer</footer>
+
+<style>
+    Nav {
+        background: rgba(0, 0, 1, 2);
+        padding-top: 1rem;
+        padding-bottom: 1rem;
+    }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// Consult https://kit.svelte.dev//integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
