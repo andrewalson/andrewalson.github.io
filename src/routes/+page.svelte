c9365e0801f0764b7a6bd28db4c8c246a34c00bb
andrewalson <83783605+andrewalson@users.noreply.github.com>
2023-09-26 18:46:26 -0400

    Remove base path.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script>
 
-  import { base } from "$app/paths";
+//   import { base } from "$app/paths";
 
 </script>
 
 <h1>Andrew Alson</h1>
 
 <div id="about" class="container detail">
-    <img src={base + '/andrew-calcifer.JPG'} alt="Andrew & Calcifer">
+    <img src='/andrew-calcifer.JPG' alt="Andrew & Calcifer">
     <br>
 </div>
